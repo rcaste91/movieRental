5bc5ed1744646bae7ca354cb437d1e815719b2d4
Ronald Castellon <rcaste91@gmail.com>
2020-10-07 12:10:03 -0600

    commit inicial

---
+++ src/test/java/com/rcaste/movieRental/MovieRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcaste.movieRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
